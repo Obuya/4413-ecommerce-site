fc5570b576aedca480e6fd1c6a3a760259eff761
Obuya <ayubosman12@hotmail.com>
2022-12-11 06:55:39 -0500

    testing

--- client/components/dashboard/Dashboard.js
+++ client/components/dashboard/Dashboard.js
@@ -17,7 +17,7 @@ function Dashboard({products, setProducts}){
   }
 
   return (
-    <div className='grid grid-cols-5 bg-gray-100 h-full'>
+    <div className='grid md:grid-cols-1 sm:grid-cols-1 lg:grid-cols-5 m-5'>
       <div className='col-span-1 h-full'>
         <div className='m-5 bg-white rounded-lg p-5 border'>
           <h1 className='text-center font-bold mb-2'>Sort</h1>
@@ -63,10 +63,10 @@ function Dashboard({products, setProducts}){
           <h1 className='text-center font-bold mb-2'>Brands</h1>
           <form>
             {// Brands
-              _.uniqBy(products.filter(product => filterCategory && product.details.type === filterCategory), 'details.brand')
+              _.uniqBy(products, 'details.brand')
                 .map(brand => (
                   <div className='flex gap-x-2' key={brand.details.brand}> 
-                    <input name="price" type={"checkbox"} onClick={() => setFilterBrand(brand.details.brand)} />
+                    <input name="price" type={"radio"} onClick={() => setFilterBrand(brand.details.brand)} />
                     <label className='font-medium'>{brand.details.brand}</label>
                   </div>
                 )
@@ -77,7 +77,7 @@ function Dashboard({products, setProducts}){
       </div>
 
       <div className='col-span-4'>
-        <div className='grid grid-cols-3 gap-5 p-5'>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8 lg:grid-cols-5  m-5'>
           {
             products 
               ? (
@@ -114,49 +114,21 @@ function ProductDisplay(product){
     if (rating.score) sum += parseInt(rating.score)
   }
   const average_rating = Math.round(sum / ratings.length)
+  const img = imageURLs.split(",", )[0]
 
   const img = imageURLs.split(',')[0]
   return (
-    <Link href={`/products/${_id}`}>
-      <div key={_id} className='border rounded-lg px-5 bg-white'>
-        <div className='h-40 w-full mb-5 mt-10 flex justify-center'>
-          <img src={img} className=' object-cover h-40' />
+    <Link href={`/products/${_id}`} className="group border border-black-500 rounded">
+        <div  key={_id} className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-white-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src = {img} className="mt-2 object-scale-down h-48 w-96 group-hover:opacity-75"/>
         </div>
-
-        <div className='text-[14px] font-light'>{name}</div>
-
-        <div>${price}</div>
-
-        <div className='flex justify-between pb-5'>
-          <div className='flex gap-x-2 items-center'>
-            <div className='flex'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill={average_rating >= 1 ? "#FFD700" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" fill={average_rating >= 2 ? "#FFD700" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" fill={average_rating >= 3 ? "#FFD700" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" fill={average_rating >= 4 ? "#FFD700" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" fill={average_rating >= 5 ? "#FFD700" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-              </svg>
-            </div>
-            <div className='text-[12px] text-black text-opacity-70'>{ratings.length} Reviews</div>
-          </div>
-          <div>
-            {
-              quantity > 0
-                ? "Available"
-                : "Not Available"
-            }
-          </div>
-        </div>
-      </div>
-    </Link>
+        <p className="px-2 mt-5 text-lg font-medium text-gray-900">${price}</p>
+        <h3 className="p-2 text-sm text-gray-700">{name}</h3>
+        
+</Link>
+  
   )
 }
+
+
+
