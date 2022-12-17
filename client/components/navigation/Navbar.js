13249005738d8620d26d981bc35b48c2675e1777
Obuya <ayubosman12@hotmail.com>
2022-12-16 22:01:05 -0500

    did it get fixed?

--- client/components/navigation/Navbar.js
+++ client/components/navigation/Navbar.js
@@ -17,23 +17,6 @@ function Navbar({search=true, loginAndCart=true}){
         <Link href={"/"}>
           <h1 className="mt-3 text-white font-bold">Home</h1>
         </Link>
-
-        {search && (
-          <div className="flex items-center bg-white rounded-lg">
-            {/* <div className="px-5 bg-white h-full flex items-center rounded-l-lg">
-              {' '}
-            </div> */}
-            <input 
-              className="h-full px-5 rounded-l-lg"
-              placeholder="Search for something"
-            />
-            <button className=" h-full items-center flex px-5">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-              </svg>
-            </button>
-          </div>
-        )}
         {loginAndCart && (
           <div className="flex gap-x-5">
             <Link href={!user ? `/login?from=${path.slice(1)}` : "/profile"}>
